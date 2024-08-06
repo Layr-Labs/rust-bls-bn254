247f2b36d1eb01ab33435f8415af29a498601a4f
supernovahs <supernovahs@proton.me>
2024-08-06 21:08:41 +0530

    Add cids multiplication

---
+++ src/arith.rs
@@ -0,0 +1,278 @@
+use ark_bn254::Fr;
+
+pub const Q_INV_VEG: u64 = 14042775128853446655;
+pub const Q0: u64 = 4891460686036598785;
+pub const Q1: u64 = 2896914383306846353;
+pub const Q2: u64 = 13281191951274694749;
+pub const Q3: u64 = 3486998266802970665;
+
+pub fn cids_multiplication(x: Fr, y: Fr) -> Fr {
+    let mut t0: u64;
+    let mut t1: u64;
+    let mut t2: u64;
+    let mut t3: u64;
+    let mut u0;
+    let mut u1: u64;
+    let mut u2: u64;
+    let mut u3: u64;
+
+    {
+        let mut c0: u64;
+        let mut c1: u64;
+        let mut c2: u64;
+
+        let v = x.0 .0[0];
+        (u0, t0) = mul_64(v, y.0 .0[0]);
+        (u1, t1) = mul_64(v, y.0 .0[1]);
+        (u2, t2) = mul_64(v, y.0 .0[2]);
+        (u3, t3) = mul_64(v, y.0 .0[3]);
+        (t1, c0) = add_64(u0, t1, 0);
+        (t2, c0) = add_64(u1, t2, c0);
+        (t3, c0) = add_64(u2, t3, c0);
+        (c2, _) = add_64(u3, 0, c0);
+
+        let m = t0.wrapping_mul(Q_INV_VEG);
+
+        (u0, c1) = mul_64(m, Q0);
+        (_, c0) = add_64(t0, c1, 0);
+        (u1, c1) = mul_64(m, Q1);
+        (t0, c0) = add_64(t1, c1, c0);
+        (u2, c1) = mul_64(m, Q2);
+        (t1, c0) = add_64(t2, c1, c0);
+        (u3, c1) = mul_64(m, Q3);
+
+        (t2, c0) = add_64(0, c1, c0);
+        (u3, _) = add_64(u3, 0, c0);
+        (t0, c0) = add_64(u0, t0, 0);
+        (t1, c0) = add_64(u1, t1, c0);
+        (t2, c0) = add_64(u2, t2, c0);
+        (c2, _) = add_64(c2, 0, c0);
+        (t2, c0) = add_64(t3, t2, 0);
+        (t3, _) = add_64(u3, c2, c0);
+    }
+
+    {
+        let mut c0: u64;
+        let mut c1: u64;
+        let mut c2: u64;
+
+        let v = x.0 .0[1];
+        (u0, c1) = mul_64(v, y.0 .0[0]);
+        (t0, c0) = add_64(c1, t0, 0);
+        (u1, c1) = mul_64(v, y.0 .0[1]);
+        (t1, c0) = add_64(c1, t1, c0);
+        (u2, c1) = mul_64(v, y.0 .0[2]);
+        (t2, c0) = add_64(c1, t2, c0);
+        (u3, c1) = mul_64(v, y.0 .0[3]);
+        (t3, c0) = add_64(c1, t3, c0);
+
+        (c2, _) = add_64(0, 0, c0);
+        (t1, c0) = add_64(u0, t1, 0);
+        (t2, c0) = add_64(u1, t2, c0);
+        (t3, c0) = add_64(u2, t3, c0);
+        (c2, _) = add_64(u3, c2, c0);
+
+        let m = t0.wrapping_mul(Q_INV_VEG);
+
+        (u0, c1) = mul_64(m, Q0);
+        (_, c0) = add_64(t0, c1, 0);
+        (u1, c1) = mul_64(m, Q1);
+        (t0, c0) = add_64(t1, c1, c0);
+        (u2, c1) = mul_64(m, Q2);
+        (t1, c0) = add_64(t2, c1, c0);
+        (u3, c1) = mul_64(m, Q3);
+
+        (t2, c0) = add_64(0, c1, c0);
+        (u3, _) = add_64(u3, 0, c0);
+        (t0, c0) = add_64(u0, t0, 0);
+        (t1, c0) = add_64(u1, t1, c0);
+        (t2, c0) = add_64(u2, t2, c0);
+        (c2, _) = add_64(c2, 0, c0);
+        (t2, c0) = add_64(t3, t2, 0);
+        (t3, _) = add_64(u3, c2, c0);
+    }
+
+    {
+        let mut c0: u64;
+        let mut c1: u64;
+        let mut c2: u64;
+
+        let v = x.0 .0[2];
+
+        (u0, c1) = mul_64(v, y.0 .0[0]);
+        (t0, c0) = add_64(c1, t0, 0);
+        (u1, c1) = mul_64(v, y.0 .0[1]);
+        (t1, c0) = add_64(c1, t1, c0);
+        (u2, c1) = mul_64(v, y.0 .0[2]);
+        (t2, c0) = add_64(c1, t2, c0);
+        (u3, c1) = mul_64(v, y.0 .0[3]);
+        (t3, c0) = add_64(c1, t3, c0);
+
+        (c2, _) = add_64(0, 0, c0);
+        (t1, c0) = add_64(u0, t1, 0);
+        (t2, c0) = add_64(u1, t2, c0);
+        (t3, c0) = add_64(u2, t3, c0);
+        (c2, _) = add_64(u3, c2, c0);
+
+        let m = t0.wrapping_mul(Q_INV_VEG);
+
+        (u0, c1) = mul_64(m, Q0);
+        (_, c0) = add_64(t0, c1, 0);
+        (u1, c1) = mul_64(m, Q1);
+        (t0, c0) = add_64(t1, c1, c0);
+        (u2, c1) = mul_64(m, Q2);
+        (t1, c0) = add_64(t2, c1, c0);
+        (u3, c1) = mul_64(m, Q3);
+
+        (t2, c0) = add_64(0, c1, c0);
+        (u3, _) = add_64(u3, 0, c0);
+        (t0, c0) = add_64(u0, t0, 0);
+        (t1, c0) = add_64(u1, t1, c0);
+        (t2, c0) = add_64(u2, t2, c0);
+        (c2, _) = add_64(c2, 0, c0);
+        (t2, c0) = add_64(t3, t2, 0);
+        (t3, _) = add_64(u3, c2, c0);
+    }
+
+    {
+        let mut c0: u64;
+        let mut c1: u64;
+        let mut c2: u64;
+
+        let v = x.0 .0[3];
+
+        (u0, c1) = mul_64(v, y.0 .0[0]);
+        (t0, c0) = add_64(c1, t0, 0);
+        (u1, c1) = mul_64(v, y.0 .0[1]);
+        (t1, c0) = add_64(c1, t1, c0);
+        (u2, c1) = mul_64(v, y.0 .0[2]);
+        (t2, c0) = add_64(c1, t2, c0);
+        (u3, c1) = mul_64(v, y.0 .0[3]);
+        (t3, c0) = add_64(c1, t3, c0);
+
+        (c2, _) = add_64(0, 0, c0);
+        (t1, c0) = add_64(u0, t1, 0);
+        (t2, c0) = add_64(u1, t2, c0);
+        (t3, c0) = add_64(u2, t3, c0);
+        (c2, _) = add_64(u3, c2, c0);
+
+        let m = t0.wrapping_mul(Q_INV_VEG);
+
+        (u0, c1) = mul_64(m, Q0);
+        (_, c0) = add_64(t0, c1, 0);
+        (u1, c1) = mul_64(m, Q1);
+        (t0, c0) = add_64(t1, c1, c0);
+        (u2, c1) = mul_64(m, Q2);
+        (t1, c0) = add_64(t2, c1, c0);
+        (u3, c1) = mul_64(m, Q3);
+
+        (t2, c0) = add_64(0, c1, c0);
+        (u3, _) = add_64(u3, 0, c0);
+        (t0, c0) = add_64(u0, t0, 0);
+        (t1, c0) = add_64(u1, t1, c0);
+        (t2, c0) = add_64(u2, t2, c0);
+        (c2, _) = add_64(c2, 0, c0);
+        (t2, c0) = add_64(t3, t2, 0);
+        (t3, _) = add_64(u3, c2, c0);
+    }
+
+    let mut z: Fr = Fr::default();
+
+    z.0 .0[0] = t0;
+    z.0 .0[1] = t1;
+    z.0 .0[2] = t2;
+    z.0 .0[3] = t3;
+
+    if !smaller_than_modulus(z) {
+        let mut b: u64;
+        (z.0 .0[0], b) = sub_64(z.0 .0[0], Q0, 0);
+        (z.0 .0[1], b) = sub_64(z.0 .0[1], Q1, 0);
+        (z.0 .0[2], b) = sub_64(z.0 .0[2], Q2, 0);
+        (z.0 .0[3], b) = sub_64(z.0 .0[3], Q3, 0);
+    }
+
+    z
+}
+
+/// mul_64 https://pkg.go.dev/math/bits#Mul64
+pub fn mul_64(x: u64, y: u64) -> (u64, u64) {
+    let product: u128 = (x as u128) * (y as u128);
+    let u1: u64 = (product >> 64) as u64;
+    let t1: u64 = product as u64;
+    (u1, t1)
+}
+
+/// add_64 https://pkg.go.dev/math/bits#Add64
+pub fn add_64(x: u64, y: u64, carry: u64) -> (u64, u64) {
+    // Perform the addition with carry
+    let (sum1, carry1) = x.overflowing_add(y);
+    let (sum2, carry2) = sum1.overflowing_add(carry);
+
+    // Calculate the final carry
+    let carry_out = (carry1 as u64) + (carry2 as u64);
+
+    (sum2, carry_out)
+}
+
+pub fn sub_64(x: u64, y: u64, borrow: u64) -> (u64, u64) {
+    let diff = x.wrapping_sub(y).wrapping_sub(borrow);
+    let borrow_out = ((!x & y) | ((!x ^ y) & diff)) >> 63;
+    (diff, borrow_out)
+}
+
+pub fn smaller_than_modulus(z: Fr) -> bool {
+    return z.0 .0[3] < Q3
+        || (z.0 .0[3] == Q3
+            && (z.0 .0[2] < Q2
+                || (z.0 .0[2] == Q2
+                    && (z.0 .0[1] < Q1 || (z.0 .0[1] == Q1 && (z.0 .0[0] < Q0))))));
+}
+
+// Test vector from https://pkg.go.dev/math/bits#Mul64
+#[test]
+fn test_mul64() {
+    let a: u64 = 9223372036854775808;
+    let b: u64 = 2;
+
+    let s = mul_64(a, b);
+    assert_eq!(1, s.0);
+    assert_eq!(0, s.1);
+
+    let c: u64 = 12;
+    let d: u64 = 12;
+
+    let s = mul_64(c, d);
+    assert_eq!(0, s.0);
+    assert_eq!(144, s.1);
+}
+
+/// Test vector https://pkg.go.dev/math/bits#Sub64
+#[test]
+fn test_sub_64() {
+    let a: u64 = 23;
+    let b: u64 = 12;
+
+    let s = sub_64(a, b, 0);
+
+    assert_eq!(11, s.0);
+}
+
+/// Test vector from https://pkg.go.dev/math/bits#Add64
+#[test]
+fn test_add_64() {
+    let x: u64 = 12;
+    let y: u64 = 23;
+    let z: u64 = 0;
+
+    let s = add_64(x, y, z); // (d1,carry)
+
+    assert_eq!(35, s.0); // d1 =35
+    assert_eq!(0, s.1); // carry bit
+
+    let a: u64 = 33;
+    let b: u64 = 21;
+
+    let ss = add_64(a, b, s.1); // (d0,_)
+
+    assert_eq!(ss.0, 54);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,18 @@
 
 extern crate alloc;
 
-use ark_ec::{pairing::PairingOutput, AffineRepr, CurveGroup};
+use ark_bn254::{Bn254, Fq, Fr, G1Affine, G1Projective, G2Affine, G2Projective};
+use ark_ec::{
+    pairing::{Pairing, PairingOutput},
+    AffineRepr, CurveGroup,
+};
+use ark_ff::{BigInteger256, Field, PrimeField};
+use ark_std::One;
 use errors::BLSError;
 use num_bigint::BigUint;
-use ark_std::One;
-use ark_bn254::{Bn254, Fq, Fr, G1Affine, G1Projective, G2Affine, G2Projective};
-use ark_ec::pairing::Pairing;
-use ark_ff::{BigInteger256, Field, PrimeField};
 use sha2::{Digest, Sha256};
 
+pub mod arith;
 pub mod errors;
 
 pub fn pairing(u: G2Affine, v: G1Affine) -> PairingOutput<Bn254> {
@@ -20,7 +23,7 @@ pub fn pairing(u: G2Affine, v: G1Affine) -> PairingOutput<Bn254> {
 fn hash_to_curve(digest: &[u8]) -> G1Affine {
     let one = Fq::one();
     let three = Fq::from(3u64);
-    
+
     let mut hasher = Sha256::new();
     hasher.update(digest);
     let hashed_result = hasher.finalize();
@@ -29,7 +32,12 @@ fn hash_to_curve(digest: &[u8]) -> G1Affine {
     let mut x = {
         let big_int = BigUint::from_bytes_be(&hashed_result);
         let mut bytes = [0u8; 32];
-        big_int.to_bytes_be().iter().rev().enumerate().for_each(|(i, &b)| bytes[i] = b);
+        big_int
+            .to_bytes_be()
+            .iter()
+            .rev()
+            .enumerate()
+            .for_each(|(i, &b)| bytes[i] = b);
         Fq::from_le_bytes_mod_order(&bytes)
     };
 
@@ -56,7 +64,8 @@ pub fn sign(sk: Fr, message: &[u8]) -> Result<G1Affine, BLSError> {
     let sk_int: BigInteger256 = sk.into();
     let r = q.mul_bigint(sk_int);
 
-    if !r.into_affine().is_on_curve() || !r.into_affine().is_in_correct_subgroup_assuming_on_curve() {
+    if !r.into_affine().is_on_curve() || !r.into_affine().is_in_correct_subgroup_assuming_on_curve()
+    {
         return Err(BLSError::SignatureNotInSubgroup);
     }
 
@@ -64,15 +73,13 @@ pub fn sign(sk: Fr, message: &[u8]) -> Result<G1Affine, BLSError> {
 }
 
 pub fn verify(public_key: G2Affine, message: &[u8], signature: G1Affine) -> bool {
-
-    if !signature.is_in_correct_subgroup_assuming_on_curve() || 
-    !signature.is_on_curve() {
+    if !signature.is_in_correct_subgroup_assuming_on_curve() || !signature.is_on_curve() {
         return false;
     }
 
     let q = hash_to_curve(message);
-    let c1 = pairing( public_key, q);
-    let c2 = pairing( G2Affine::generator(), signature);
+    let c1 = pairing(public_key, q);
+    let c2 = pairing(G2Affine::generator(), signature);
     c1 == c2
 }
 
@@ -81,7 +88,8 @@ pub fn aggregate_signatures(signatures: &[G1Affine]) -> Result<G1Affine, BLSErro
         return Err(BLSError::SignatureListEmpty);
     }
 
-    let signatures_in_projective: Vec<G1Projective> = signatures.iter()
+    let signatures_in_projective: Vec<G1Projective> = signatures
+        .iter()
         .map(|sig| {
             let proj = G1Projective::from(*sig);
             if !sig.is_on_curve() || !sig.is_in_correct_subgroup_assuming_on_curve() {
@@ -103,7 +111,8 @@ pub fn aggregate_public_keys(public_keys: &[G2Affine]) -> Result<G2Affine, BLSEr
         return Err(BLSError::PublicKeyListEmpty);
     }
 
-    let public_keys_in_projective: Vec<G2Projective> = public_keys.iter()
+    let public_keys_in_projective: Vec<G2Projective> = public_keys
+        .iter()
         .map(|pk| {
             let proj = G2Projective::from(*pk);
             if !pk.is_on_curve() || !pk.is_in_correct_subgroup_assuming_on_curve() {
@@ -125,8 +134,8 @@ pub fn aggregate_public_keys(public_keys: &[G2Affine]) -> Result<G2Affine, BLSEr
 mod test {
     use super::*;
     use ark_bn254::G2Projective;
-    use ark_std::{ops::Mul, test_rng};
     use ark_ff::UniformRand;
+    use ark_std::{ops::Mul, test_rng};
 
     #[test]
     fn test_aggregate_and_verify() {
@@ -134,14 +143,16 @@ mod test {
 
         // Generate private keys and corresponding public keys
         let private_keys: Vec<Fr> = (0..500).map(|_| Fr::rand(&mut rng)).collect();
-        let public_keys: Vec<G2Affine> = private_keys.iter()
+        let public_keys: Vec<G2Affine> = private_keys
+            .iter()
             .map(|sk| (G2Affine::generator() * sk).into_affine())
             .collect();
 
         let message = b"Test message";
 
         // Sign the message with each private key
-        let signatures: Vec<G1Affine> = private_keys.iter()
+        let signatures: Vec<G1Affine> = private_keys
+            .iter()
             .map(|sk| sign(*sk, message).unwrap())
             .collect();
 
@@ -156,10 +167,11 @@ mod test {
 
     #[test]
     fn test_generic() {
-        
         let mut rng = ark_std::test_rng();
         let sk = Fr::rand(&mut rng);
-        let pubkey = G2Projective::from(G2Affine::generator()).mul(sk).into_affine();
+        let pubkey = G2Projective::from(G2Affine::generator())
+            .mul(sk)
+            .into_affine();
 
         let message = "random".as_bytes();
         let message2 = "random2".as_bytes();
@@ -185,7 +197,10 @@ mod test {
         let empty_signatures: Vec<G1Affine> = vec![];
         let result = aggregate_signatures(&empty_signatures);
         assert!(result.is_err());
-        assert_eq!(result.unwrap_err().to_string(), BLSError::SignatureListEmpty.to_string());
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            BLSError::SignatureListEmpty.to_string()
+        );
 
         // Test with an invalid signature
         let mut invalid_signature = G1Affine::rand(&mut rng);
@@ -194,7 +209,10 @@ mod test {
         invalid_signatures.push(invalid_signature);
         let result = aggregate_signatures(&invalid_signatures);
         assert!(result.is_err());
-        assert_eq!(result.unwrap_err().to_string(), BLSError::SignatureNotInSubgroup.to_string());
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            BLSError::SignatureNotInSubgroup.to_string()
+        );
     }
 
     #[test]
@@ -212,7 +230,10 @@ mod test {
         let empty_public_keys: Vec<G2Affine> = vec![];
         let result = aggregate_public_keys(&empty_public_keys);
         assert!(result.is_err());
-        assert_eq!(result.unwrap_err().to_string(), BLSError::PublicKeyListEmpty.to_string());
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            BLSError::PublicKeyListEmpty.to_string()
+        );
 
         // Test with an invalid public key
         let mut invalid_public_key = G2Affine::rand(&mut rng);
@@ -221,6 +242,9 @@ mod test {
         invalid_public_keys.push(invalid_public_key);
         let result = aggregate_public_keys(&invalid_public_keys);
         assert!(result.is_err());
-        assert_eq!(result.unwrap_err().to_string(), BLSError::PublicKeyNotInSubgroup.to_string());
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            BLSError::PublicKeyNotInSubgroup.to_string()
+        );
     }
 }
